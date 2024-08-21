e1fac2a5951dc872e9818dadc599c8aaf8cef56b
Pawel Rucki <12943682+pawelru@users.noreply.github.com>
2024-08-21 11:48:30 +0200

    export create_shinylive_url

--- R/parse_url.R
+++ R/parse_url.R
@@ -5,7 +5,15 @@
 #'
 #' @param code (`character(1)`) A string with app code.
 #' @return (`character(1)`) Shinylive app url.
+#'
+#' @export
+#'
+#' @examples
+#' code <- "this is your app code as a string"
+#' create_shinylive_url(code)
 create_shinylive_url <- function(code) {
+  stopifnot(is.character(code) && length(code) == 1)
+
   # implementation based on "Create ShinyLive Link" feature of Shiny VSCode extension
   # https://github.com/posit-dev/shiny-vscode/blob/80560bf36d516ff89dffe88bd9a28cee9edd4d43/src/shinylive.ts#L499
   files <- list(
